6ac6c3fdf650978d7cea61219a4aac0e81befeb6
deepu13defender <deepukrishna25@gmail.com>
2022-07-01 15:46:17 +0530

    gooood

--- boot_it/settings.py
+++ boot_it/settings.py
@@ -91,11 +91,11 @@ AUTH_USER_MODEL = 'accounts.Account'
 DATABASES = {
    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'boot_it', 
-        'USER': 'postgres', 
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'), 
+        'PORT': config('DB_PORT'),
     }
 }
 
