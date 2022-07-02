08a4da503ac3a66282df864e7b9f9bfa2782a05d
deepu13defender <deepukrishna25@gmail.com>
2022-07-02 12:02:24 +0530

    hope

--- boot_it/urls.py
+++ boot_it/urls.py
@@ -15,15 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('master/',include('master.urls')),
-    path('accounts/',include('accounts.urls')),   
-
+    path('accounts/' ,include('accounts.urls')),  
     path('orders/',include('orders.urls')),
     path('',include('store.urls')),
     
