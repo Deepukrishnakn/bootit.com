fee50bc00b8126ce11a6ba2f288d6a39e932d531
deepu13defender <deepukrishna25@gmail.com>
2022-07-02 11:57:24 +0530

    welcome

--- boot_it/urls.py
+++ boot_it/urls.py
@@ -21,11 +21,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/',include('accounts.urls')),   
     path('master/',include('master.urls')),
+    path('accounts/',include('accounts.urls')),   
+
     path('orders/',include('orders.urls')),
     path('',include('store.urls')),
-   
     
-    
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
