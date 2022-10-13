cae9f8cc7948276f5e8f1aecdb760582423c5dc1
deepukrishna kn <deepukrishna25@gmsil.com>
2022-10-13 15:06:28 +0530

    database configured

--- boot_it/settings.py
+++ boot_it/settings.py
@@ -83,15 +83,28 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'boot_it', 
+#         'USER': 'postgres', 
+#         'PASSWORD': '1234',
+#         'HOST': '127.0.0.1', 
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'boot_it', 
-        'USER': 'postgres', 
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1', 
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+       'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost',
         'PORT': '5432',
     }
+
 }
 
 
