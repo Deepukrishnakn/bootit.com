d928d6f7ab0c34312e1524e8d6650f3cbb1b218f
deepukrishna kn <deepukrishna25@gmsil.com>
2022-10-13 12:18:39 +0530

    db

--- boot_it/settings.py
+++ boot_it/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'boot_it', 
         'USER': 'postgres', 
-        'PASSWORD': 'admin',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
