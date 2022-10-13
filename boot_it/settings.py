e99394b59e106d45a5a301e8b6dcb581b5ecc9a2
deepukrishna kn <deepukrishna25@gmsil.com>
2022-10-13 12:24:24 +0530

    db

--- boot_it/settings.py
+++ boot_it/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-62ku5v@!9ppm0t_5*&npxou*2cfp-@o(((t8e$tf0ig9rkw0#6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
