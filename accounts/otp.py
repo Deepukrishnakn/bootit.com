577e49ae805f4059acd94125dd8930df7fb9c15a
deepukrishna kn <deepukrishna25@gmsil.com>
2022-10-13 12:52:32 +0530

    tiwlio

--- accounts/otp.py
+++ accounts/otp.py
@@ -1,9 +1,9 @@
-import os
 from twilio.rest import Client
 from twilio.base.exceptions import TwilioRestException
+from django.conf import settings
 
-client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
-verify = client.verify.services(os.environ['TWILIO_VERIFY_SERVICE_SID'])
+client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+verify = client.verify.services(settings.TWILIO_VERIFY_SERVICE_SID)
 
 
 def send(phone_number):
